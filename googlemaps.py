8ac0183a5496e615e06174a7eedc5cceac24860d
SyuanBo <godspeed88168@gmail.com>
2021-11-26 00:29:42 +0800

    2021.11.26 update

--- googlemaps.py
+++ googlemaps.py
@@ -18,6 +18,7 @@ MAX_WAIT = 10
 MAX_RETRY = 5
 MAX_SCROLLS = 40
 
+
 class GoogleMapsScraper:
 
     def __init__(self, debug=False):
@@ -46,10 +47,13 @@ class GoogleMapsScraper:
         tries = 0
         while not clicked and tries < MAX_RETRY:
             try:
-                #if not self.debug:
+                # if not self.debug:
                 #    menu_bt = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.cYrDcjyGO77__container')))
-                #else:
-                menu_bt = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-value=\'Sort\']')))
+                # else:
+                # menu_bt = wait.until(EC.element_to_be_clickable(
+                #     (By.XPATH, '//button[@data-value=\'Sort\']')))
+                menu_bt = wait.until(EC.element_to_be_clickable(
+                    (By.XPATH, '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[1]/div[1]/div[2]/div/div[1]/span[1]/span/span[1]/span[2]/span[1]/button')))
                 menu_bt.click()
 
                 clicked = True
@@ -63,39 +67,39 @@ class GoogleMapsScraper:
                 return -1
 
         #  element of the list specified according to ind
-        recent_rating_bt = self.driver.find_elements_by_xpath('//li[@role=\'menuitemradio\']')[ind]
+        recent_rating_bt = self.driver.find_elements_by_xpath(
+            '//*[@id="pane"]/div/div[1]/div/div/div[2]/div[7]/div[2]/button/span/span')[0]
         recent_rating_bt.click()
-
+        recent_rating_bt_select = self.driver.find_elements_by_xpath(
+            '//*[@id="action-menu"]/ul/li')[ind]
+        recent_rating_bt_select.click()
         # wait to load review (ajax call)
         time.sleep(5)
 
         return 0
 
-
     def get_reviews(self, offset):
-
-        # scroll to load reviews
-
-        # wait for other reviews to load (ajax)
-        time.sleep(4)
-
-        self.__scroll()
-
 
         # expand review text
         self.__expand_reviews()
 
         # parse reviews
         response = BeautifulSoup(self.driver.page_source, 'html.parser')
-        rblock = response.find_all('div', class_='section-review-content')
+        # pane > div > div.Yr7JMd-pane-content.cYB2Ge-oHo7ed > div > div > div.siAUzd-neVct.section-scrollbox.cYB2Ge-oHo7ed.cYB2Ge-ti6hGc > div:nth-child(9) > div:nth-child(1) > div > div.ODSEW-ShBeI-content
+        rblock = response.find_all('div', class_='ODSEW-ShBeI-content')
+        print("get_reviews")
         parsed_reviews = []
         for index, review in enumerate(rblock):
             if index >= offset:
                 parsed_reviews.append(self.__parse(review))
-                print(self.__parse(review))
+                # print(self.__parse(review))
+
+        # scroll to load reviews
+        self.__scroll()
+        # wait for other reviews to load (ajax)
+        time.sleep(2)
 
         return parsed_reviews
-
 
     def get_account(self, url):
 
@@ -110,24 +114,29 @@ class GoogleMapsScraper:
 
         return place_data
 
-
     def __parse(self, review):
 
         item = {}
 
-        id_review = review.find('button', class_='section-review-action-menu')['data-review-id']
-        username = review.find('div', class_='section-review-title').find('span').text
+        id_review = review.find(
+            'button', class_='ODSEW-ShBeI-JIbuQc-menu ODSEW-ShBeI-JIbuQc-menu-SfQLQb-title')['data-review-id']
+        username = review.find(
+            'div', class_='ODSEW-ShBeI-title').find('span').text
 
         try:
-            review_text = self.__filter_string(review.find('span', class_='section-review-text').text)
+            review_text = self.__filter_string(review.find(
+                'span', class_='ODSEW-ShBeI-text').text)
         except Exception as e:
             review_text = None
 
-        rating = float(review.find('span', class_='section-review-stars')['aria-label'].split(' ')[1])
-        relative_date = review.find('span', class_='section-review-publish-date').text
+        rating = float(review.find(
+            'span', class_='ODSEW-ShBeI-H1e3jb')['aria-label'].split(' ')[1])
+        relative_date = review.find(
+            'span', class_='ODSEW-ShBeI-RgZmSc-date').text
 
         try:
-            n_reviews_photos = review.find('div', class_='section-review-subtitle').find_all('span')[1].text
+            n_reviews_photos = review.find(
+                'div', class_='section-review-subtitle').find_all('span')[1].text
             metadata = n_reviews_photos.split('\xe3\x83\xbb')
             if len(metadata) == 3:
                 n_photos = int(metadata[2].split(' ')[0].replace('.', ''))
@@ -161,17 +170,18 @@ class GoogleMapsScraper:
 
         return item
 
-
     def __parse_place(self, response):
 
         place = {}
         try:
-            place['overall_rating'] = float(response.find('div', class_='gm2-display-2').text.replace(',', '.'))
+            place['overall_rating'] = float(response.find(
+                'div', class_='gm2-display-2').text.replace(',', '.'))
         except:
             place['overall_rating'] = 'NOT FOUND'
 
         try:
-            place['n_reviews'] = int(response.find('div', class_='gm2-caption').text.replace('.', '').replace(',','').split(' ')[0])
+            place['n_reviews'] = int(response.find(
+                'div', class_='gm2-caption').text.replace('.', '').replace(',', '').split(' ')[0])
         except:
             place['n_reviews'] = 0
 
@@ -180,7 +190,10 @@ class GoogleMapsScraper:
     # expand review description
     def __expand_reviews(self):
         # use XPath to load complete reviews
-        links = self.driver.find_elements_by_xpath('//button[@class=\'section-expand-review blue-link\']')
+        # links = self.driver.find_elements_by_xpath('//button[@class=\'section-expand-review blue-link\']')
+        # pane > div > div.Yr7JMd-pane-content.cYB2Ge-oHo7ed > div > div > div.siAUzd-neVct.section-scrollbox.cYB2Ge-oHo7ed.cYB2Ge-ti6hGc > div:nth-child(9) > div:nth-child(184) > div > div.ODSEW-ShBeI-content > div:nth-child(3) > jsl > button
+        links = self.driver.find_elements_by_css_selector(
+            "div.ODSEW-ShBeI-RWgCYc jsl button")
         for l in links:
             l.click()
         time.sleep(2)
@@ -188,21 +201,28 @@ class GoogleMapsScraper:
     # load more reviews
     def more_reviews(self):
         # use XPath to load complete reviews
-        #allxGeDnJMl__text gm2-button-alt
-        #<button ved="1i:1,t:18519,e:0,p:kPkcYIz-Dtql-QaL1YawDw:1969" jstcache="1202" jsaction="pane.reviewChart.moreReviews" class="gm2-button-alt jqnFjrOWMVU__button-blue" jsan="7.gm2-button-alt,7.jqnFjrOWMVU__button-blue,0.ved,22.jsaction">14 reviews</button>
-        #<button aria-label="14 reviews" vet="3648" jsaction="pane.rating.moreReviews" jstcache="1010" class="widget-pane-link" jsan="7.widget-pane-link,0.aria-label,0.vet,0.jsaction">14 reviews</button>
-        links = self.driver.find_elements_by_xpath('//button[@jsaction=\'pane.reviewChart.moreReviews\']')
+        # allxGeDnJMl__text gm2-button-alt
+        # <button ved="1i:1,t:18519,e:0,p:kPkcYIz-Dtql-QaL1YawDw:1969" jstcache="1202" jsaction="pane.reviewChart.moreReviews" class="gm2-button-alt jqnFjrOWMVU__button-blue" jsan="7.gm2-button-alt,7.jqnFjrOWMVU__button-blue,0.ved,22.jsaction">14 reviews</button>
+        # <button aria-label="14 reviews" vet="3648" jsaction="pane.rating.moreReviews" jstcache="1010" class="widget-pane-link" jsan="7.widget-pane-link,0.aria-label,0.vet,0.jsaction">14 reviews</button>
+        links = self.driver.find_elements_by_xpath(
+            '//button[@jsaction=\'pane.reviewChart.moreReviews\']')
         print('LINKS HERE', links)
         for l in links:
             l.click()
         time.sleep(2)
 
-
     def __scroll(self):
-        scrollable_div = self.driver.find_element_by_css_selector('div.section-layout.section-scrollbox.scrollable-y.scrollable-show')
-        self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
-        #self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # 進到評論畫面之後往下滑
+        scrollable_div = self.driver.find_element_by_css_selector(
+            "div.siAUzd-neVct.section-scrollbox.cYB2Ge-oHo7ed.cYB2Ge-ti6hGc")
+        self.driver.execute_script(
+            'arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
+        # scrollable_div = self.driver.find_element(By.XPATH("//*[@id='pane']/div/div[1]"))
 
+        # self.driver.execute_script(
+        #     'var q=document.documentElement.scrollTop=10000')
+        # self.driver.execute_script(
+        #     "window.scrollTo(0, document.body.scrollHeight);")
 
     def __get_logger(self):
         # create logger
@@ -214,7 +234,8 @@ class GoogleMapsScraper:
         fh.setLevel(logging.DEBUG)
 
         # create formatter
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s')
 
         # add formatter to ch
         fh.setFormatter(formatter)
@@ -223,7 +244,6 @@ class GoogleMapsScraper:
         logger.addHandler(fh)
 
         return logger
-
 
     def __get_driver(self, debug=False):
         options = Options()
@@ -234,13 +254,13 @@ class GoogleMapsScraper:
             options.add_argument("--window-size=1366,768")
 
         options.add_argument("--disable-notifications")
-        options.add_argument("--lang=en-GB")
+        options.add_argument("--lang=zh-TW")
         input_driver = webdriver.Chrome(chrome_options=options)
 
         return input_driver
 
-
     # util function to clean special characters
+
     def __filter_string(self, str):
         strOut = str.replace('\r', ' ').replace('\n', ' ').replace('\t', ' ')
         return strOut
--- scraper.py
+++ scraper.py
@@ -7,12 +7,16 @@ from termcolor import colored
 import time
 
 
-ind = {'most_relevant' : 0 , 'newest' : 1, 'highest_rating' : 2, 'lowest_rating' : 3 }
-HEADER = ['id_review', 'caption', 'relative_date', 'retrieval_date', 'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user']
-HEADER_W_SOURCE = ['id_review', 'caption', 'relative_date','retrieval_date', 'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user', 'url_source']
+ind = {'most_relevant': 0, 'newest': 1,
+       'highest_rating': 2, 'lowest_rating': 3}
+HEADER = ['id_review', 'caption', 'relative_date', 'retrieval_date',
+          'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user']
+HEADER_W_SOURCE = ['id_review', 'caption', 'relative_date', 'retrieval_date',
+                   'rating', 'username', 'n_review_user', 'n_photo_user', 'url_user', 'url_source']
+
 
 def csv_writer(source_field, ind_sort_by, path='data/'):
-    outfile= ind_sort_by + '_gm_reviews.csv'
+    outfile = ind_sort_by + '_google_map_reviews.csv'
     targetfile = open(path + outfile, mode='w', encoding='utf-8', newline='\n')
     writer = csv.writer(targetfile, quoting=csv.QUOTE_MINIMAL)
 
@@ -26,13 +30,20 @@ def csv_writer(source_field, ind_sort_by, path='data/'):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Google Maps reviews scraper.')
-    parser.add_argument('--N', type=int, default=100, help='Number of reviews to scrape')
-    parser.add_argument('--i', type=str, default='urls.txt', help='target URLs file')
-    parser.add_argument('--sort_by', type=str, default='newest', help='sort by most_relevant, newest, highest_rating or lowest_rating')
-    parser.add_argument('--place', dest='place', action='store_true', help='Scrape place metadata')
-    parser.add_argument('--debug', dest='debug', action='store_true', help='Run scraper using browser graphical interface')
-    parser.add_argument('--source', dest='source', action='store_true', help='Add source url to CSV file (for multiple urls in a single file)')
+    parser = argparse.ArgumentParser(
+        description='Google Maps reviews scraper.')
+    parser.add_argument('--N', type=int, default=100,
+                        help='Number of reviews to scrape')
+    parser.add_argument('--i', type=str, default='urls.txt',
+                        help='target URLs file')
+    parser.add_argument('--sort_by', type=str, default='newest',
+                        help='sort by most_relevant, newest, highest_rating or lowest_rating')
+    parser.add_argument('--place', dest='place',
+                        action='store_true', help='Scrape place metadata')
+    parser.add_argument('--debug', dest='debug', action='store_true',
+                        help='Run scraper using browser graphical interface')
+    parser.add_argument('--source', dest='source', action='store_true',
+                        help='Add source url to CSV file (for multiple urls in a single file)')
     parser.set_defaults(place=False, debug=False, source=False)
 
     args = parser.parse_args()
@@ -55,11 +66,12 @@ if __name__ == '__main__':
 
                     if ind[args.sort_by] == 0:
                         scraper.more_reviews()
-                        
+
                     while n < args.N:
                         print(colored('[Review ' + str(n) + ']', 'cyan'))
-                        reviews = scraper.get_reviews(n)
 
+                        reviews = scraper.get_reviews(n)
+                        # print(reviews)
                         for r in reviews:
                             row_data = list(r.values())
                             if args.source:
